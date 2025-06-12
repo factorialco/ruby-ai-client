0d199fccfb1ec6182d600b4833665071cd175a0d
Andrey Oskin <andrey.oskin@factorial.co>
2025-06-12 16:15:16 +0300

    simplify: remove parametrized Agent, move output where used

--- lib/ai/agent.rb
+++ lib/ai/agent.rb
@@ -4,122 +4,74 @@ module Ai
   class Agent
     extend T::Sig
 
-    class Instance
-      extend T::Sig
-      extend T::Generic
+    sig { returns(String) }
+    attr_reader :agent_name
 
-      Output = type_member
+    sig { returns(Ai::Client) }
+    attr_reader :client
 
-      sig { returns(String) }
-      attr_reader :agent_name
+    sig { params(agent_name: String, client: Ai::Client).void }
+    def initialize(agent_name:, client:)
+      @agent_name = agent_name
+      @client = client
+    end
 
-      sig { returns(T.nilable(T::Class[Output])) }
-      attr_reader :output_class
+    sig do
+      params(
+        messages: T::Array[Ai::Message],
+        runtime_context: T::Hash[String, T.anything],
+        max_retries: Integer,
+        max_steps: Integer
+      ).returns(Ai::GenerateTextResult)
+    end
+    def generate_text(messages:, runtime_context: {}, max_retries: 2, max_steps: 5)
+      options = { runtime_context: runtime_context, max_retries: max_retries, max_steps: max_steps }
 
-      sig { returns(Ai::Client) }
-      attr_reader :client
-
-      sig do
-        params(
-          agent_name: String,
-          client: Ai::Client,
-          output_class: T.nilable(T::Class[Output])
-        ).void
-      end
-      def initialize(agent_name:, client:, output_class: nil)
-        @agent_name = agent_name
-        @client = client
-        @output_class = output_class
-      end
-
-      sig do
-        params(
-          messages: T::Array[Ai::Message],
-          runtime_context: T::Hash[String, T.anything],
-          max_retries: Integer,
-          max_steps: Integer
-        ).returns(Ai::GenerateTextResult)
-      end
-      def generate_text(messages:, runtime_context: {}, max_retries: 2, max_steps: 5)
-        options = {
-          runtime_context: runtime_context,
-          max_retries: max_retries,
-          max_steps: max_steps
-        }
-
-        data = client.generate(agent_name, messages: messages, options: options)
-        TypeCoerce[Ai::GenerateTextResult].new.from(data, raise_coercion_error: false)
-      end
-
-      sig do
-        params(
-          messages: T::Array[Ai::Message],
-          runtime_context: T::Hash[String, T.anything],
-          max_retries: Integer,
-          max_steps: Integer
-        ).returns(GenerateObjectResult[Output])
-      end
-      def generate_object(messages:, runtime_context: {}, max_retries: 2, max_steps: 5)
-        if output_class.nil?
-          raise "You need to provide an output class for #{agent_name} when calling generate_object"
-        end
-
-        output = Ai::StructToJsonSchema.convert(T.cast(output_class, T.class_of(T::Struct)))
-
-        options = {
-          runtime_context: runtime_context,
-          max_retries: max_retries,
-          max_steps: max_steps,
-          output: output
-        }
-
-        data = client.generate(agent_name, messages: messages, options: options)
-
-        object = TypeCoerce[output_class].from(data['object'])
-        TypeCoerce[GenerateObjectResult[Output]]
-          .new
-          .from(data, raise_coercion_error: false)
-          .with(object: object)
-      end
+      data = client.generate(agent_name, messages: messages, options: options)
+      TypeCoerce[Ai::GenerateTextResult].new.from(data, raise_coercion_error: false)
     end
 
     sig do
       type_parameters(:O)
         .params(
-          agent_name: String,
+          messages: T::Array[Ai::Message],
           output_class: T.all(T::Class[T.type_parameter(:O)], T::Class[T::Struct]),
-          client: Ai::Client
+          runtime_context: T::Hash[String, T.anything],
+          max_retries: Integer,
+          max_steps: Integer
         )
-        .returns(Ai::Agent::Instance[T.all(T.type_parameter(:O), T::Struct)])
+        .returns(GenerateObjectResult[T.type_parameter(:O)])
     end
-    def self.[](agent_name, output_class, client: Ai.client)
-      Instance.new(agent_name: agent_name, output_class: output_class, client: client)
-    end
+    def generate_object(messages:, output_class:, runtime_context: {}, max_retries: 2, max_steps: 5)
+      output = Ai::StructToJsonSchema.convert(T.cast(output_class, T.class_of(T::Struct)))
 
-    sig do
-      params(
-        agent_name: String,
-        messages: T::Array[Ai::Message],
-        runtime_context: T::Hash[String, T.anything],
-        max_retries: Integer,
-        max_steps: Integer,
-        client: Ai::Client
-      ).returns(Ai::GenerateTextResult)
-    end
-    def self.generate_text(
-      agent_name,
-      messages:,
-      runtime_context: {},
-      max_retries: 2,
-      max_steps: 5,
-      client: Ai.client
-    )
-      Instance.new(agent_name: agent_name, client: client).generate_text(
-        messages: messages,
+      options = {
         runtime_context: runtime_context,
         max_retries: max_retries,
-        max_steps: max_steps
-      )
+        max_steps: max_steps,
+        output: output
+      }
+
+      data = client.generate(agent_name, messages: messages, options: options)
+
+      object = TypeCoerce[output_class].from(data['object'])
+      TypeCoerce[GenerateObjectResult]
+        .new
+        .from(data, raise_coercion_error: false)
+        .with(object: object)
     end
+  end
+
+  class TestOutput < T::Struct
+    const :name, String
+    const :age, Integer
+  end
+
+  sig { void }
+  def test
+    a = Agent.new(agent_name: 'test', client: Ai::Clients::Test.new)
+    r = a.generate_object(messages: [Ai.user_message('Create a person')], output_class: TestOutput)
+
+    r.object.name.age
   end
 end
